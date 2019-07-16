1b8f24db39b23172f9768b30eaf230891a353c61
kondouyutaka <39979735+kondouyutaka@users.noreply.github.com>
2019-07-16 13:33:19 +0900

    Add files via upload

--- htdocs/gazou.php
+++ htdocs/gazou.php
@@ -4,18 +4,18 @@
 <?php
 //データベース接続設定
 $dbServer = '127.0.0.1';
-$dbName = 'mydb';
+$dbName = $_SERVER['MYSQL_DB'];
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
-$dbUser = 'test';
-$dbPass = 'pass';
+$dbUser = $_SERVER['MYSQL_USER'];
+$dbPass = $_SERVER['MYSQL_PASSWORD'];
 
 //データベースへの接続
 $db = new PDO($dsn, $dbUser, $dbPass);
 
 
 header("Content-type: image/jpg");
-$stmt = $db->query("select * from mytable2 WHERE id = '$id' order by rand()  limit 1");
+$stmt = $db->query("select * from maindb WHERE cookingID ='$id'");
 $record = $stmt->fetch();
-$gazou = $record['gazou'];
+$gazou = $record['image'];
 echo $gazou;
 ?>
